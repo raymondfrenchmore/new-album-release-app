5622602849d0412cbdf2cb270abb2c83db14a611
Raymond Frenchmore <raymondfrenchmore@live.com>
2018-06-07 09:40:25 -0700

    final project

---
+++ src/Results/Results.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import Search from '../Search/Search';
+
+class Results extends Component {
+  render() {
+    return (
+        <div className="artistDetails"></div>
+    );
+  }
+}
+
+export default Results;
