f01f3cbac836c0ab949da0000fa539e64fdfeaff
Raymond Frenchmore <raymondfrenchmore@live.com>
2018-06-08 22:36:13 -0700

    add component

---
+++ src/Release/Release.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import Results from '../Results/Results';
+import './Release.css';
+
+class Release extends Component {
+  render() {
+    return (
+        <div className="artistDetails">
+            <article><span class="helper"></span><a href="https://www.discogs.com/David-Bowie-Low/release/11638558" target="_blank"><img src="https://img.discogs.com/9sY2P2DoVBWIWAOQlPJRs6AFm1M=/fit-in/600x535/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-11638558-1519845059-7935.jpeg.jpg" /></a><div class="textArea"><p class="artistName">David Bowie</p><p class="releaseTitle">Low</p></div></article>
+            <article><span class="helper"></span><a href="https://www.discogs.com/David-Bowie-Low/release/11638558" target="_blank"><img src="https://img.discogs.com/9sY2P2DoVBWIWAOQlPJRs6AFm1M=/fit-in/600x535/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-11638558-1519845059-7935.jpeg.jpg" /></a><div class="textArea"><p class="artistName">David Bowie</p><p class="releaseTitle">Low</p></div></article>
+            <article><span class="helper"></span><a href="https://www.discogs.com/David-Bowie-Low/release/11638558" target="_blank"><img src="https://img.discogs.com/9sY2P2DoVBWIWAOQlPJRs6AFm1M=/fit-in/600x535/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-11638558-1519845059-7935.jpeg.jpg" /></a><div class="textArea"><p class="artistName">David Bowie</p><p class="releaseTitle">Low</p></div></article>
+      </div>
+    );
+  }
+}
+
+export default Release;
